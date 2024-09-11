ba73735acede258d7d5a64b28925bcd2a9e5c085
Christopher Tate <computate@computate.org>
2024-09-11 04:42:35 +0000

    Adding docs, vars and notebooks for deployment in OpenShift AI

---
+++ doc/getting-started/openshift-ai/generate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd $HOME/ai-telemetry/doc/getting-started/openshift-ai
+eval $(./vars.py)
+cd $COMPUTATE_SRC
+mvn dependency:build-classpath -Dmdep.outputFile=$COMPUTATE_SRC/config/cp.txt -q
+env WATCH=false VARS_PATH=$VARS_PATH COMPUTATE_SRC=$COMPUTATE_SRC SITE_LANG=$SITE_LANG java -cp "$(cat $COMPUTATE_SRC/config/cp.txt):$COMPUTATE_SRC/target/classes" org.computate.frFR.java.RegarderRepertoire
