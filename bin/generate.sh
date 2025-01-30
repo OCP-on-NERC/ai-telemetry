fbfed8c2e54c1ceae1a8cb4369f957d3cdf325af
Christopher Tate <computate@computate.org>
2025-01-30 20:34:23 +0000

    Fixing the deploy workbenches automation

--- bin/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/ctate/.local/src/ai-telemetry
-env VARS_PATH=/home/ctate/.local/src/ai-telemetry/vars.yaml \
-  COMPUTATE_SRC=/home/ctate/.local/src/computate \
-  COMPUTATE_VERTX_SRC=/home/ctate/.local/src/computate-vertx \
-  SITE_LANG=enUS \
-  /home/ctate/.local/src/computate/bin/enUS/generate.sh
