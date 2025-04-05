b3dfc51d56466b005bae4b60f0ad85a3777ccb7c
Christopher Tate <computate@computate.org>
2025-04-04 23:26:29 -0600

    Adding ansible for deploying workbenches

---
+++ kustomize/overlays/moc/roles/deploy-workbenches/templates/index.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd {{ SITE_SRC }}
+env VARS_PATH={{ SITE_SRC }}/vars.yaml \
+  COMPUTATE_SRC={{ COMPUTATE_SRC }} \
+  COMPUTATE_VERTX_SRC={{ COMPUTATE_VERTX_SRC }} \
+  SITE_LANG=enUS \
+  {{ COMPUTATE_SRC }}/bin/enUS/index.sh
